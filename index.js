a3b67e14ee2449066a0d99feeef00e40baba71de
Adeline Lefebvre <adelinelefebvre@iphone-de-adeline.home>
2021-07-29 16:27:09 +0200

    message

--- index.js
+++ index.js
@@ -9,19 +9,16 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://adeline_admin:W0P5Qvg3UPagHBjZ@cluster0.1l5wx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 cloudinary.config({
-  cloud_name: "didine",
-  api_key: 773545984858169,
-  api_secret: "4FTdLtxTMcGWkJwZPHiDah_s9zU",
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 });
 
 const userRoutes = require("./routes/user");
