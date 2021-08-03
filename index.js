165390c468772c5d73923bebb45b6114be20ce9a
Adeline Lefebvre <adelinelefebvre@MacBook-Pro-de-Adeline.home>
2021-08-03 14:12:37 +0200

    message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(formidable());
@@ -31,6 +32,28 @@ const userRoutes = require("./routes/user");
 const offerRoutes = require("./routes/offer");
 app.use(userRoutes);
 app.use(offerRoutes);
+
+app.post("/payment", async (req, res) => {
+  try {
+    const response = await stripe.charges.create({
+      amount: req.fields.price * 100, // en centimes
+      currency: "eur",
+      description: "La description du produit...",
+      source: req.fields.stripeToken,
+    });
+    console.log("La réponse de Stripe ====> ", response);
+    if (response.status === "succeeded") {
+      res.status(200).json({ message: "Paiement validé" });
+    } else {
+      res.status(400).json({ message: "An error occured" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// TODO
+// Sauvegarder la transaction dans une BDD MongoDB
 
 app.all("*", (req, res) => {
   res.status(404).json({ message: "Page not found" });
