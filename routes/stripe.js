06a3bf4d9df4497334903ace77b55b20d3ec5d33
Adeline Lefebvre <adelinelefebvre@iphone-de-adeline.home>
2021-08-03 21:47:51 +0200

    message

--- routes/stripe.js
+++ routes/stripe.js
@@ -8,6 +8,7 @@ router.post("/payment", async (req, res) => {
   try {
     const response = await stripe.charges.create({
       amount: req.fields.amount * 100,
+      currency: "eur",
       product_name: req.fields.title,
       source: req.fields.stripeToken,
     });
